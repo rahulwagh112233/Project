c39efdc623544b56c51a648d262e1288fa7f2027
gsujith <gsujith@virtusa.com>
2024-12-04 20:54:31 +0530

    adding the aapla project

---
+++ apla market/apla market/Online-Food-Ordering-System-in-PHP-main/admin/include/footer.php
@@ -0,0 +1,2 @@
+ <footer class="footer" style="text-align: center;">eat healthy be healthy apli mati apli mansa - <a href=""></a> </footer>
+ 
---
+++ apla market/apla market/Online-Food-Ordering-System-in-PHP-main/include/footer.php
@@ -0,0 +1,40 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>ambad chaufuli old jalna.</p>
+                          <h5>Phone:+91 9581100981</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>eat healthy be healthy vist the shop</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
